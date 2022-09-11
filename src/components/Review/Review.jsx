a86be6e9f95fecbd78fc07297c0d5404b85a8da5
LAPTOP-C0717LCE\princ <anupkar07.ak@gmail.com>
2022-09-11 18:10:20 +0530

    Milestone4 prompt

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -5,7 +5,10 @@ import { StorageCard } from './StroageCard';
 import { Card as SecurityCard } from '../Security/Security';
 import styles from '../Content/Card.module.css';
 import style from './Review.module.css';
+import { SuccessPrompt } from '../SuccessPrompt/SuccessPrompt';
+import { useState } from 'react';
 export const Review = () => {
+  const [success,setSuccess]=useState(false)
   const { images, instenceType, securities, handleNavChange } = useContext(HvcContext);
   const navigate = useNavigate();
 
@@ -50,88 +53,94 @@ export const Review = () => {
   };
   return (
     <>
-      <div className={style.review_main_container}>
-        <button onClick={generateJSON} className={style.generate_btn}>
-          Generate JSON
-        </button>
-        <div style={{ width: '95%' }}>
-          <div className={style.heading}>
-            <p className={style.Image_heading}>Image</p>
-            <span className={style.edit_span}>EDIT</span>
-          </div>
-          <div>
-            <div className={styles.card}>
-              <div className={styles.leftSide}>
-                {/* image */}
-                <div className={styles.image} />
-
-                {/* title and description */}
-                <div className={styles.details}>
-                  <p>{IMAGE.name}</p>
-                  <p>{IMAGE.description}</p>
-                </div>
+      {success && <SuccessPrompt/>}
+       
+        <div style={{pointerEvents: success ? 'none':'auto',opacity: success ? 0.2:1}}>
+          <div className={style.review_main_container}>
+            <button onClick={generateJSON} className={style.generate_btn}>
+              Generate JSON
+            </button>
+            <div style={{ width: '95%' }}>
+              <div className={style.heading}>
+                <p className={style.Image_heading}>Image</p>
+                <span className={style.edit_span}>EDIT</span>
               </div>
-              <div className={styles.selection}>
-                <p>{IMAGE.bit.name}</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div>
-          <div>
-            <p className={style.Image_heading}>Instance</p>
-          </div>
-          <div>
-            <div className={style.card}>
-              <div className={styles.leftSide}>
-                {/* image */}
+              <div>
+                <div className={styles.card}>
+                  <div className={styles.leftSide}>
+                    {/* image */}
+                    <div className={styles.image} />
 
-                {/* title and description */}
-                <div className={styles.details}>
-                  <p>{instenceType}</p>
-                  <p>
-                    {images[1].name} - {images[1].bit.name}
-                  </p>
-                  <p>
-                    {images[2].name} - {images[2].bit.name}
-                  </p>
+                    {/* title and description */}
+                    <div className={styles.details}>
+                      <p>{IMAGE.name}</p>
+                      <p>{IMAGE.description}</p>
+                    </div>
+                  </div>
+                  <div className={styles.selection}>
+                    <p>{IMAGE.bit.name}</p>
+                  </div>
                 </div>
               </div>
             </div>
+            <div>
+              <div>
+                <p className={style.Image_heading}>Instance</p>
+              </div>
+              <div>
+                <div className={style.card}>
+                  <div className={styles.leftSide}>
+                    {/* image */}
+
+                    {/* title and description */}
+                    <div className={styles.details}>
+                      <p>{instenceType}</p>
+                      <p>
+                        {images[1].name} - {images[1].bit.name}
+                      </p>
+                      <p>
+                        {images[2].name} - {images[2].bit.name}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div>
+                <p className={style.Image_heading}>Storage</p>
+              </div>
+              <div>
+                {images.slice(3).map((each) => {
+                  return <StorageCard key={each.id} data={each} />;
+                })}
+              </div>
+            </div>
+            <div>
+              <div>
+                <p className={style.Image_heading}>Security Groups</p>
+              </div>
+              <div>
+                {securities.map((each) => {
+                  return <SecurityCard key={each.id} data={each} isDefault={true} id={each.id} isShow={true} />;
+                })}
+              </div>
+            </div>
+          </div>
+          <div className={style.Button_main}>
+            <div className={style.btn_cancel}>
+              <p>Cancel</p>
+            </div>
+            <div className={style.btn_back}>
+              <p>Back</p>
+            </div>
+            <div className={style.btn} onClick={()=>setSuccess(true)}>
+              <p>Launch</p>
+            </div>
           </div>
         </div>
-        <div>
-          <div>
-            <p className={style.Image_heading}>Storage</p>
-          </div>
-          <div>
-            {images.slice(3).map((each) => {
-              return <StorageCard key={each.id} data={each} />;
-            })}
-          </div>
-        </div>
-        <div>
-          <div>
-            <p className={style.Image_heading}>Security Groups</p>
-          </div>
-          <div>
-            {securities.map((each) => {
-              return <SecurityCard key={each.id} data={each} isDefault={true} id={each.id} isShow={true} />;
-            })}
-          </div>
-        </div>
-      </div>
-      <div className={style.Button_main}>
-        <div className={style.btn_cancel}>
-          <p>Cancel</p>
-        </div>
-        <div className={style.btn_back}>
-          <p>Back</p>
-        </div>
-        <div className={style.btn}>
-          <p>Launch</p>
-        </div>
-      </div>
+      
+      
     </>
   );
 };
